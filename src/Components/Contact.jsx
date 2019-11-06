699ec5bde4b01661f26359d96e429d735518190f
Heather Zurek <heather.zurek@gmail.com>
2019-11-06 14:57:08 -0800

    UGH MARGINS

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Contact(){
     return(
-        <div>
+        <div className="contact-container">
             <h2>Get in Touch</h2>
             <p>Do you have questions? Want to grab coffee and chat? Let's connect!</p>
             {/* <hr/>
