6e7c45e1d87987f25f940fe95885c483c1d47704
Heather Zurek <heather.zurek@gmail.com>
2019-11-06 09:39:11 -0800

    remove commented out code

--- src/Components/Intro.jsx
+++ src/Components/Intro.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import Links from './Links';
-// import Name from './Name';
-// import logo from './Images/logo.png';
 
 export default function Intro() {
     return(
         <div>
             <h1 class="name-intro">Heather Zurek</h1>
-            <p class="title-intro">Software and Support Engineer
-            <Links /></p>
+            <div className="title-intro">
+            <p>Software and Support Engineer</p>
+            <Links />
+            </div>
             <hr/>
         </div>
     );
