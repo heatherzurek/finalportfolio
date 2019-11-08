a8bb115b578fbbb534835ce1517a51c4e0ccf245
Heather Zurek <heather.zurek@gmail.com>
2019-11-07 18:39:03 -0800

    edit about.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -6,7 +6,7 @@ export default function About() {
     return(
         <div>
             <h2 className="about-container">About</h2>
-            <p className="p-format">My pronouns are she/her. I'm a .NET Support Engineer, with a background in C#, .NET, JavaScript, Node.js, and React. I work closely with .NET frameworks and SQL, but have an excitement and passion for frontend development. I enjoy discovering new technologies, and there is never a shortage of things to learn. I am based in the beautiful Pacific Northwest, where I spend my summers hiking, camping, and exploring National Parks with my husband, and my spring/fall/winters trying to stay dry by staying inside and watching tv shows, programming or playing games(console or board!).</p>
+            <p className="p-format">I'm a .NET Support Engineer, with a background in C#, .NET, JavaScript, Node.js, and React. I work closely with .NET frameworks and SQL, but have an excitement and passion for mobile first  frontend development. I enjoy discovering new technologies, and there is never a shortage of things to learn. I am based in the beautiful Pacific Northwest, where I spend my summers hiking, camping, and exploring National Parks with my husband, and my spring/fall/winters trying to stay dry by staying inside and watching tv shows, programming or playing games(console or board!).</p>
             <p className="faChev"><FontAwesomeIcon icon={ faChevronDown } size="lg"/></p>
         </div>
     );
