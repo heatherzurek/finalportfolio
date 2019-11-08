a3d7f88cb60ef82fb8e0f74c6722fbd13fd6c163
Heather Zurek <heather.zurek@gmail.com>
2019-11-08 09:47:22 -0800

    comment out img

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import me from "./Images/me.png";
+// import me from "./Images/me.png";
 
 
 export default function Contact() {
@@ -8,7 +8,7 @@ export default function Contact() {
       <h2>Get in Touch</h2>
       <p>Do you have questions? Want to grab coffee and chat? Let's connect!</p>
       <div className="contact-image">
-      <img src={me} alt="woman and mountains" />
+      {/* <img src={me} alt="woman and mountains" /><img src={me} alt="woman and mountains" /> */}
       </div>
     </div>
   );
